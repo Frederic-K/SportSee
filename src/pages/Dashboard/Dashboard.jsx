a4cda849435d3e32c2eaafd0653cd88357b5972f
Frederic.K <frederic.kreuder@gmail.com>
2023-05-25 18:21:14 +0200

    Set html for dashboard

--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -1,8 +1,29 @@
+import Aside from '../../components/Aside/Aside'
+
 function Dashboard() {
   return (
-    <section>
-      <h1>TEST Dashboard</h1>
-    </section>
+    <div className="dashboard">
+      <Aside />
+      <section className="dashboard__content">
+        <h1 className="dashboard__title">TEST Dashboard</h1>
+        <div className="dashboard__datas">
+          <div classNama="dasboard__datas--charts">
+            <div className="chart__dailyActivity"></div>
+            <div className="chart__activity">
+              <div className="chart__activity--averageSessions"></div>
+              <div className="chart__activity--performance"></div>
+              <div className="chart__activity--score"></div>
+            </div>
+          </div>
+          <div className="dashboard__datas--nutrition">
+            <div className="datas__nutrition--calories"></div>
+            <div className="datas__nutrition--protein"></div>
+            <div className="datas__nutrition--carbs"></div>
+            <div className="datas__nutrition--fat"></div>
+          </div>
+        </div>
+      </section>
+    </div>
   )
 }
 
