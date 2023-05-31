a653feadebed3a461ebf3cd971f23e9ec070fffd
Frederic.K <frederic.kreuder@gmail.com>
2023-05-31 18:53:35 +0200

    Set perf score chart

--- src/components/Chart/PerformanceChart/PerformanceChart.jsx
+++ src/components/Chart/PerformanceChart/PerformanceChart.jsx
@@ -1,10 +1,18 @@
+import {
+  PolarAngleAxis,
+  // PolarRadiusAxis,
+  PolarGrid,
+  Radar,
+  RadarChart,
+  ResponsiveContainer,
+} from 'recharts'
+
 import usePerformance from '../../../services/API/usePerformance'
 import SpinLoader from '../../Loader/SpinLoader'
 
-export default function DailyScoreChart({ userId }) {
+export default function PerformanceChart({ userId }) {
   const { isLoading, data, error } = usePerformance('user-performance', userId)
-
-  console.log('dataChart', data)
+  // console.log('dataChart', data)
 
   if (error) {
     return <div>Erreur de chargement...</div>
@@ -12,7 +20,35 @@ export default function DailyScoreChart({ userId }) {
 
   return (
     <div className="chart__activity--performance">
-      {isLoading ? <SpinLoader /> : <div className="performance">Tadam</div>}
+      {isLoading ? (
+        <SpinLoader />
+      ) : (
+        <div className="chart__performance">
+          <ResponsiveContainer width="100%" height="100%">
+            <RadarChart cx="50%" cy="50%" outerRadius={80} data={data}>
+              <PolarGrid radialLines={false} />
+              <PolarAngleAxis
+                dataKey="kind"
+                stroke="white"
+                dy={3}
+                tickLine={false}
+                tick={{
+                  fontSize: 10,
+                  fontWeight: 400,
+                  fill: 'white',
+                }}
+              />
+              {/* <PolarRadiusAxis angle={0} stroke="white" /> */}
+              <Radar
+                dataKey="value"
+                fill="#ff0000"
+                fillOpacity={0.7}
+                stroke="#ff0000"
+              />
+            </RadarChart>
+          </ResponsiveContainer>
+        </div>
+      )}
     </div>
   )
 }
